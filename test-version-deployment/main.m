5ed4ea2b94193d6106d71173be5eab8939f0f7f3
Swizzlr <git@swizzlr.co>
2013-12-05 18:18:37 +0000

    Initial commit

---
+++ test-version-deployment/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test-version-deployment
+//
+//  Created by swizzlr on 05/12/2013.
+//  Copyright (c) 2013 swizzlr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
